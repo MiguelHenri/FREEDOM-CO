7e293a12c2e8f12be7a0341fc13c640f47f8d40e
MiguelHenri <miguelbrahen@gmail.com>
2024-06-07 09:08:32 -0300

    adding put method and little fixes

--- projects/project2/backend/app.py
+++ projects/project2/backend/app.py
@@ -1,32 +1,76 @@
 from flask import Flask, request, jsonify
-from models.db import db
-from models.item import Item
+from models.DataBase import db
+from models.StoreItem import StoreItem
+from config import Config
 
 # Initializing app and db with configs
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object(Config)
 db.init_app(app)
 
 @app.route('/api/items', methods=['GET'])
 def get_items():
-    items = Item.query.all()
+    # Querying all itens
+    items = StoreItem.query.all()
     return jsonify([item.to_dict() for item in items])
+
+@app.route('/api/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    # Querying item from id
+    item = StoreItem.query.get_or_404(item_id)
+    return jsonify(item.to_dict())
 
 @app.route('/api/items', methods=['POST'])
 def create_item():
     data = request.json
-    new_item = Item(
+    
+    # Validating required fields
+    required_fields = ['image', 'title', 'description', 'value']
+    for field in required_fields:
+        if not data.get(field):
+            return jsonify({'error': f'Field {field} is required.'}), 400
+    
+    new_item = StoreItem(
         image=data['image'],
         title=data['title'],
         description=data['description'],
         value=data['value'],
         oldValue=data.get('oldValue', None),
         tagColor=data.get('tagColor', None),
-        tag=data.get('tag', None)
+        tag=data.get('tag', None),
+        size_quantity_pairs=data.get('size_quantity_pairs', {})
     )
     db.session.add(new_item)
     db.session.commit()
     return jsonify(new_item.to_dict()), 201
+
+
+@app.route('/api/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    # Querying item from id
+    item = StoreItem.query.get_or_404(item_id)
+    data = request.json
+
+    # Updating values
+    if 'image' in data:
+        item.image = data['image']
+    if 'title' in data:
+        item.title = data['title']
+    if 'description' in data:
+        item.description = data['description']
+    if 'value' in data:
+        item.value = data['value']
+    if 'oldValue' in data:
+        item.oldValue = data['oldValue']
+    if 'tagColor' in data:
+        item.tagColor = data['tagColor']
+    if 'tag' in data:
+        item.tag = data['tag']
+    if 'size_quantity_pairs' in data:
+        item.size_quantity_pairs = data['size_quantity_pairs']
+
+    db.session.commit()
+    return jsonify(item.to_dict()), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- projects/project2/backend/models/StoreItem.py
+++ projects/project2/backend/models/StoreItem.py
@@ -1,16 +1,19 @@
 from .DataBase import db
 from uuid import uuid4
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.mutable import MutableDict
 
 class StoreItem(db.Model):
-    __tablename__ = 'items'  # Nome da tabela existente no banco de dados
+    __tablename__ = 'items'  # Database (PG) table name
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4()))
-    image = db.Column(db.String(500), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    value = db.Column(db.String(50), nullable=False)
-    oldValue = db.Column(db.String(50), nullable=True)
-    tagColor = db.Column(db.String(50), nullable=True)
-    tag = db.Column(db.String(50), nullable=True)
+    value = db.Column(db.String(100), nullable=False)
+    oldValue = db.Column(db.String(100))
+    tagColor = db.Column(db.String(50))
+    tag = db.Column(db.String(50))
+    size_quantity_pairs = db.Column(MutableDict.as_mutable(JSONB), default=dict)
 
     def to_dict(self):
         return {
@@ -21,5 +24,6 @@ class StoreItem(db.Model):
             'value': self.value,
             'oldValue': self.oldValue,
             'tagColor': self.tagColor,
-            'tag': self.tag
+            'tag': self.tag,
+            'size_quantity_pairs': self.size_quantity_pairs
         }
