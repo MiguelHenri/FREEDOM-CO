4a8b2a62e2be80ba67b836798edb53a9013c0d43
MiguelHenri <miguelbrahen@gmail.com>
2024-06-07 09:11:49 -0300

    adding delete method

--- projects/project2/backend/app.py
+++ projects/project2/backend/app.py
@@ -72,5 +72,13 @@ def update_item(item_id):
     db.session.commit()
     return jsonify(item.to_dict()), 200
 
+@app.route('/api/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    # Querying item from id
+    item = StoreItem.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return '', 204
+
 if __name__ == '__main__':
     app.run(debug=True)
