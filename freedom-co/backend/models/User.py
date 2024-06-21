3dfbb4c80ad11112886c66f90d655848bbebc273
MiguelHenri <miguelbrahen@gmail.com>
2024-06-21 12:51:58 -0300

    adding signup function

--- freedom-co/backend/models/User.py
+++ freedom-co/backend/models/User.py
@@ -1,5 +1,4 @@
 from .DataBase import db
-from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users' # Database (PG) table name
@@ -7,12 +6,6 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
     
     def to_dict(self):
         return {
--- freedom-co/backend/routes/users.py
+++ freedom-co/backend/routes/users.py
@@ -1,11 +1,36 @@
 from Flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
 from models.DataBase import db
 from models.User import User
 
 users_bp = Blueprint('User', __name__)
 
-@users_bp.route('/api/users/<uuid:id>', methods=['GET'])
-def get_user(id):
-    # Querying user from id
-    user = User.query.get_or_404(id)
-    return jsonify(user.to_dict())
+# @users_bp.route('/api/users/<uuid:id>', methods=['GET'])
+# def get_user(id):
+#     # Querying user from id
+#     user = User.query.get_or_404(id)
+#     return jsonify(user.to_dict())
+
+@users_bp.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Username and password are required.'}), 400
+    
+    new_user = User(
+        username=username,
+        password=generate_password_hash(password, method='bcrypt'))
+    
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'message': 'Username already exists.'}), 400
+    
+    return jsonify({'message': 'User created successfully.'}), 201
