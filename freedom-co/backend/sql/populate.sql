24e6b339bdebf8a7655286cc4aa237fd5c2bd2a2
MiguelHenri <miguelbrahen@gmail.com>
2024-07-07 17:05:19 -0300

    updating populate tables

--- freedom-co/backend/sql/populate.sql
+++ freedom-co/backend/sql/populate.sql
@@ -1,109 +1,87 @@
+-- Populating items table
 INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
 ('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Camiseta Básica', 
- 'Camiseta básica de algodão', 
- '$19.99', 
- 'tee',
- '$29.99', 
- 'green', 
- 'SALE', 
- '{"P": 10, "M": 20, "G": 15, "GG": 5}');
+    'Basic Tee', 
+    'Basic cotton t-shirt.', 
+    '$19.99', 
+    'tee',
+    '$29.99', 
+    'green', 
+    'SALE', 
+    '{"P": 10, "M": 15, "G": 10, "GG": 5}');
+
+INSERT INTO items (image, title, description, value, type, size_quantity_pairs) VALUES
+('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
+    'Over Tee', 
+    'Basic cotton oversized t-shirt.', 
+    '$24.99', 
+    'tee',
+    '{"P": 5, "M": 10, "G": 10, "GG": 5}');
+
+INSERT INTO items (image, title, description, value, type, tagColor, tag, size_quantity_pairs) VALUES
+('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
+    'New Over Tee', 
+    'New cotton oversized t-shirt.', 
+    '$29.99', 
+    'tee',
+    'blue', 
+    'NEW', 
+    '{"P": 10, "M": 20, "G": 15, "GG": 10}');
 
 INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
 ('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Calça Jeans', 
- 'Calça jeans de corte reto', 
- '$49.99', 
- 'pant',
- '$59.99', 
- 'blue', 
- 'NEW', 
- '{"P": 5, "M": 10, "G": 10, "GG": 2}');
+    'Basic Pant', 
+    'Basic jeans pant.', 
+    '$39.99', 
+    'pant',
+    '$49.99', 
+    'green', 
+    'SALE', 
+    '{"P": 10, "M": 15, "G": 10, "GG": 5}');
+
+INSERT INTO items (image, title, description, value, type, size_quantity_pairs) VALUES
+('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
+    'Baggy Pant', 
+    'Basic baggy jeans.', 
+    '$54.99', 
+    'pant',
+    '{"P": 5, "M": 10, "G": 10, "GG": 5}');
+
+INSERT INTO items (image, title, description, value, type, tagColor, tag, size_quantity_pairs) VALUES
+('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
+    'New Pant', 
+    'New baggy jeans!', 
+    '$69.99', 
+    'pant',
+    'blue', 
+    'NEW', 
+    '{"P": 10, "M": 20, "G": 15, "GG": 10}');
 
 INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
 ('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Boné Estiloso', 
- 'Boné estiloso para o dia a dia', 
- '$14.99', 
- 'accessory',
- NULL, 
- 'blue', 
- 'NEW', 
- '{"P": 0, "M": 5, "G": 5, "GG": 0}');
+    'Hat', 
+    'Basic hat.', 
+    '$19.99', 
+    'accessory',
+    '$24.99', 
+    'green', 
+    'SALE', 
+    '{"P": 10, "M": 15, "G": 10, "GG": 5}');
 
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
+INSERT INTO items (image, title, description, value, type, size_quantity_pairs) VALUES
 ('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Jaqueta de Couro', 
- 'Jaqueta de couro legítimo', 
- '$199.99', 
- 'accessory',
- '$249.99', 
- 'green', 
- 'SALE', 
- '{"P": 1, "M": 3, "G": 2, "GG": 1}');
+    'Sunglasses', 
+    'Cool sunglasses.', 
+    '$29.99', 
+    'accessory',
+    '{"P": 5, "M": 10, "G": 10, "GG": 5}');
 
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
+INSERT INTO items (image, title, description, value, type, tagColor, tag, size_quantity_pairs) VALUES
 ('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Blusa de Lã', 
- 'Blusa de lã para inverno', 
- '$39.99', 
- 'tee',
- '$49.99', 
- 'blue', 
- 'NEW', 
- '{"P": 10, "M": 15, "G": 10, "GG": 5}');
-
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
-('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Sapato Social', 
- 'Sapato social de couro', 
- '$89.99', 
- 'accessory',
- '$99.99', 
- 'green', 
- 'SALE', 
- '{"P": 2, "M": 4, "G": 6, "GG": 1}');
-
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
-('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Vestido Floral', 
- 'Vestido floral para o verão', 
- '$59.99', 
- 'accessory',
- '$69.99', 
- 'blue', 
- 'NEW', 
- '{"P": 5, "M": 10, "G": 8, "GG": 2}');
-
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
-('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Tênis Esportivo', 
- 'Tênis esportivo para corrida', 
- '$79.99', 
- 'accessory',
- '$89.99', 
- 'green', 
- 'SALE', 
- '{"P": 3, "M": 6, "G": 4, "GG": 1}');
-
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
-('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Suéter de Algodão', 
- 'Suéter confortável de algodão', 
- '$34.99', 
- 'tee',
- '$44.99', 
- 'blue', 
- 'NEW', 
- '{"P": 7, "M": 12, "G": 10, "GG": 3}');
-
-INSERT INTO items (image, title, description, value, type, oldValue, tagColor, tag, size_quantity_pairs) VALUES
-('https://civilrights.msu.edu/_assets/images/placeholder/placeholder-200x200.jpg', 
- 'Casaco de Inverno', 
- 'Casaco de inverno acolchoado', 
- '$149.99', 
- 'accessory',
- '$179.99', 
- 'green', 
- 'SALE', 
- '{"P": 4, "M": 8, "G": 6, "GG": 2}');
+    'New Gloves', 
+    'New wool gloves.', 
+    '$39.99', 
+    'accessory',
+    'blue', 
+    'NEW', 
+    '{"P": 10, "M": 20, "G": 15, "GG": 10}');
