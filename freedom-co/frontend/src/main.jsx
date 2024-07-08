f3092bb0947fb42bbb8035c25e418020b0c7b175
MiguelHenri <miguelbrahen@gmail.com>
2024-07-08 15:23:36 -0300

    fixing input focus bug

--- freedom-co/frontend/src/main.jsx
+++ freedom-co/frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
-import theme from './theme.js';
 import '@mantine/core/styles.css';
+import theme from './theme.js';
 import { CartProvider } from './contexts/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- freedom-co/frontend/src/theme.js
+++ freedom-co/frontend/src/theme.js
@@ -1,14 +1,11 @@
 import { createTheme } from "@mantine/core";
 
-export default createTheme({
+const theme = createTheme({
     colors: {
         primary: ['#f2f2f2'],
         secondary: ['#0b59de'],
         tertiary: ['#b38144'],
     },
-    primaryColor: 'primary',
-    secondaryColor: 'secondary',
-    tertiaryColor: 'tertiary',
 
     components: {
         Anchor: {
@@ -48,3 +45,5 @@ export default createTheme({
         }
     }
 });
+
+export default theme;
