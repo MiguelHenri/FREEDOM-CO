d6c1ef3a9352eddd5d376ba9b24ef35fc6aa57bf
MiguelHenri <miguelbrahen@gmail.com>
2024-07-08 13:39:50 -0300

    fixing store pages margin

--- freedom-co/frontend/src/pages/Store/Store.jsx
+++ freedom-co/frontend/src/pages/Store/Store.jsx
@@ -4,7 +4,7 @@ import StoreItemRow from "../../components/StoreItemRow";
 function Store() {
     return(
         <>
-        <Paper shadow='sm' radius='xs' ml='20px' mr='20px' p='md' withBorder>
+        <Paper shadow='sm' radius='xs' ml='20px' mr='20px' p='md' withBorder mt='md'>
             <Text ta='center' fz="25px" ff="'Lilita One', sans-serif">
                 STORE 
             </Text>
