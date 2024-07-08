a23ac0e8bf3db4a82e7abc057b0ed8df4622d5ce
MiguelHenri <miguelbrahen@gmail.com>
2024-07-08 14:18:35 -0300

    now using hover into item cards

--- freedom-co/frontend/src/components/StoreItem.jsx
+++ freedom-co/frontend/src/components/StoreItem.jsx
@@ -1,8 +1,10 @@
 import { Text, Card, Image, Title, Group, Badge } from "@mantine/core";
+import { useHover } from "@mantine/hooks";
 
 
 function StoreItem({item, ...others}) {
     const {id, image, title, description, value, oldValue, tag, tagColor} = item;
+    const { hovered, ref } = useHover();
 
     return(
         <Card 
@@ -13,39 +15,46 @@ function StoreItem({item, ...others}) {
             h='300px' w='300px'
             component='a'
             href={`store/${id}`}
+            ref={ref}
             {...others}
         >
             {image && 
                 <Card.Section>
-                    <Image src={image} alt={title} h='200px'/>
+                    <Image 
+                        src={image} 
+                        alt={title} 
+                        h={hovered ? '200px' : '300px'}
+                    />
                 </Card.Section>
             }
             
-            <Card.Section p='sm' bg='primary.0'>
-                <Group justify='space-between'>
-                    <Title order={4}>
-                        {title}
-                    </Title>
-                    {tagColor && tag && (
-                        <Badge color={tagColor} size='lg'>
-                            {tag}
-                        </Badge>
-                    )}
-                </Group>
-                <Text mb='3px'>
-                    {description}
-                </Text>
-                <Group align='center'>
-                    {oldValue && (
-                        <Text td='line-through'>
-                            {oldValue}
-                        </Text>
-                    )}
-                    <Text fw={700}>  
-                        {value}
+            {hovered &&
+                <Card.Section p='sm' bg='primary.0'>
+                    <Group justify='space-between'>
+                        <Title order={4}>
+                            {title}
+                        </Title>
+                        {tagColor && tag && (
+                            <Badge color={tagColor} size='lg'>
+                                {tag}
+                            </Badge>
+                        )}
+                    </Group>
+                    <Text mb='3px'>
+                        {description}
                     </Text>
-                </Group>
-            </Card.Section>
+                    <Group align='center'>
+                        {oldValue && (
+                            <Text td='line-through'>
+                                {oldValue}
+                            </Text>
+                        )}
+                        <Text fw={700}>  
+                            {value}
+                        </Text>
+                    </Group>
+                </Card.Section>
+            }
         </Card>
     );
 }
